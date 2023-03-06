c4ba522ee570e9f6699077b0dab7eaa8556aedf1
denghua <denghua.fm@foxmail.com>
2023-03-06 23:29:25 +0800

    add alias in next.config.js

--- next.config.js
+++ next.config.js
@@ -1,6 +1,22 @@
 /**
+ * next.config.js
  * @type {import('next').NextConfig}
  */
-module.exports = {
+const path = require('path')
+const nextConfig = {
   reactStrictMode: true,
+  swcMinify: true,
 }
+
+module.exports = {
+  ...nextConfig,
+  webpack: (config,
+    { buildId, dev, isServer, defaultLoaders, nextRuntime, webpack }) => {
+    config.resolve.alias = {
+      ...config.resolve.alias,
+      '@': path.resolve(__dirname),
+      '@public': path.resolve(__dirname, './public'),
+    }
+    return config
+  }
+};
